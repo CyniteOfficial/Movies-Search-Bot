4446ca93cc225b22a5ff3ea91933c90bd7d4cb6d
Abhivermashb <98682923+Abhivermashb@users.noreply.github.com>
2023-01-24 22:25:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "Get It From Vercel"
+URL = "https://movies-search-bot-beta.vercel.app"
 bot = Bot(TOKEN)
 
 
